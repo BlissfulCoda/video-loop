9960a2f47e068fc16647c84780036d041b1e5722
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-15 09:37:52 +0100

    added a button event listener to display video on click

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-(function(){
-    //Set UI variables
-    const videoEl = document.getElementById('video');
-    const button = document.getElementById('button');
+(function() {
+  //Set UI variables
+  const videoEl = document.getElementById('video');
+  const button = document.getElementById('button');
 
-
-    async function selectMediaStream(){
-        try{
-            const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-            videoEl.srcObject = mediaStream;
-            videoEl.onloadedmetadata = () => {
-                videoEl.play();
-            }
-        } catch(error){
-            //Catch an error
-            console.log('Whoops, error here :', error)
-        }
-
-        // button.addEventListener('click', async () => {
-        //     button.disabled = true;
-        //     await videoElement.requestPictureInPicture();
-        //     button.disabled = false;
-        // })
+  async function selectMediaStream() {
+    try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoEl.srcObject = mediaStream;
+      videoEl.onloadedmetadata = () => {
+        videoEl.play();
+      };
+    } catch (error) {
+      //Catch an error
+      console.log('Whoops, error here :', error);
     }
 
-    selectMediaStream();
-})()
+    button.addEventListener('click', async () => {
+      button.disabled = true;
+      await videoEl.requestPictureInPicture();
+      button.disabled = false;
+    });
+  }
+
+  selectMediaStream();
+})();
