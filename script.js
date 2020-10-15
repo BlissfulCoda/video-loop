3eacf1404d983783ecd1ce9a327433bc53e34fea
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-15 09:28:50 +0100

    added an async function to display video media

---
+++ script.js
@@ -0,0 +1,27 @@
+(function(){
+    //Set UI variables
+    const videoEl = document.getElementById('video');
+    const button = document.getElementById('button');
+
+
+    async function selectMediaStream(){
+        try{
+            const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+            videoEl.srcObject = mediaStream;
+            videoEl.onloadedmetadata = () => {
+                videoEl.play();
+            }
+        } catch(error){
+            //Catch an error
+            console.log('Whoops, error here :', error)
+        }
+
+        // button.addEventListener('click', async () => {
+        //     button.disabled = true;
+        //     await videoElement.requestPictureInPicture();
+        //     button.disabled = false;
+        // })
+    }
+
+    selectMediaStream();
+})()
